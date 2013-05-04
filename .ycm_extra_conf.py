58f404bf0c4c093231e3f3ac125bc4742441ba3f
Xeon Xu <xeonxu@gmail.com>
2013-05-04 22:08:26 +0800

    mac support

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -42,6 +42,8 @@ flags = [
 '-Wno-long-long',
 '-Wno-variadic-macros',
 '-fexceptions',
+'-DNDEBUG',
+'-DUSE_CLANG_COMPLETER',
 # ...and the same thing goes for the magic -x option which specifies the
 # language that the files to be compiled are written in. This is mostly
 # relevant for c++ headers.
@@ -49,6 +51,11 @@ flags = [
 '-x',
 'c',
 '-isystem',
+# This path will only work on OS X, but extra paths that don't exist are not
+# harmful
+'/System/Library/Frameworks/Python.framework/Headers',
+'-isystem',
+'src',
 '-I',
 '.',
 '-I',
