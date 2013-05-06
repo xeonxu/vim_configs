30d008006b5876f19932779edb68f75efda8e677
wu.zhang <wu.zhang@phicomm.com.cn>
2013-05-06 08:51:54 +0800

    revert mktags to linux version

--- bin/mktags.sh
+++ bin/mktags.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-(echo "!_TAG_FILE_SORTED	2	/2=foldcase/";(gfind . \( -name .svn -o -name .git -o -wholename ./classes \) -prune -o -not -iregex '.*\.\(o\|lib\|a\|jar\|gif\|jpg\|class\|exe\|dll\|pdd\|sw[op]\|xls\|doc\|pdf\|zip\|tar\|ico\|ear\|war\|dat\).*' -type f -printf "%f\t%p\t1\n" |sort -f)) > filenametags
+(echo "!_TAG_FILE_SORTED	2	/2=foldcase/";(find . \( -name .svn -o -name .git -o -wholename ./classes \) -prune -o -not -iregex '.*\.\(o\|lib\|a\|jar\|gif\|jpg\|class\|exe\|dll\|pdd\|sw[op]\|xls\|doc\|pdf\|zip\|tar\|ico\|ear\|war\|dat\).*' -type f -printf "%f\t%p\t1\n" |sort -f)) > filenametags
 
-gfind . -iregex '.*\.[chm][px]\{0,2\}' -o -iregex '.*\.cc' > gtags.file
+find . -iregex '.*\.[chm][px]\{0,2\}' -o -iregex '.*\.cc' > gtags.file
 if [ ! -e "./GTAGS" ];then
 gtags > /dev/null 2>&1
 else
