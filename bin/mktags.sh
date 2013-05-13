882042b262c632d1821601d2ecb9386f5636e0c8
Xeon Xu <xeonxu@gmail.com>
2013-05-13 22:14:47 +0800

    Add Mac support.

--- bin/mktags.sh
+++ bin/mktags.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-(echo "!_TAG_FILE_SORTED	2	/2=foldcase/";(find . \( -name .svn -o -name .git -o -wholename ./classes \) -prune -o -not -iregex '.*\.\(o\|lib\|a\|jar\|gif\|jpg\|class\|exe\|dll\|pdd\|sw[op]\|xls\|doc\|pdf\|zip\|tar\|ico\|ear\|war\|dat\).*' -type f -printf "%f\t%p\t1\n" |sort -f)) > filenametags
+(echo "!_TAG_FILE_SORTED	2	/2=foldcase/";(gfind . \( -name .svn -o -name .git -o -wholename ./classes \) -prune -o -not -iregex '.*\.\(o\|lib\|a\|jar\|gif\|jpg\|class\|exe\|dll\|pdd\|sw[op]\|xls\|doc\|pdf\|zip\|tar\|ico\|ear\|war\|dat\).*' -type f -printf "%f\t%p\t1\n" |sort -f)) > filenametags
 
-find . -iregex ".*\.[chm]" -o -iregex ".*\.[chm]pp" -o -iregex ".*\.[chm]xx" -o -iregex '.*\.cc' -o -iregex '.*\.java' -type f > gtags.file
+gfind . -iregex ".*\.[chm]" -o -iregex ".*\.[chm]pp" -o -iregex ".*\.[chm]xx" -o -iregex '.*\.cc' -o -iregex '.*\.java' -type f > gtags.file
 if [ ! -e "./GTAGS" ];then
 gtags > /dev/null 2>&1
 else
